e127f699d22a7392731d368f7a10257155bede57
Nkaleth <nseguralu@gmail.com>
2024-02-07 16:41:17 -0500

    ADD connection to mongoDB  atlas

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequilize = new Sequelize("node_eshop", "root", "nilton", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://nseguralu:Kaleth123@cluster0.kxqugrg.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-module.exports = sequilize;
+module.exports = mongoConnect;
