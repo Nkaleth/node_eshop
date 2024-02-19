b1dc30646e2e6e9f2bbf60a2715c61b6a88d276f
Nkaleth <nseguralu@gmail.com>
2024-02-19 17:41:52 -0500

    Install express session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,15 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
-  console.log(isLoggedIn);
+  // const isLoggedIn =
+  //   req.get("Cookie").split(";")[0].trim().split("=")[1] === "true";
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
   req.isLoggedIn = true;
   res.redirect("/");
 };
