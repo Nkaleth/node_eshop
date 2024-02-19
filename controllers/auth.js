cf09b0a8aa18aa6d5db314cb070347aa82f661bb
Nkaleth <nseguralu@gmail.com>
2024-02-19 12:18:54 -0500

    SET isAuthenticated variable using isLoggedIn cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,15 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
+  console.log(isLoggedIn);
   res.render("auth/login", {
     pageTitle: "Login",
-    path: "/products",
-    isAuthenticated: req.isLoggedIn,
+    path: "/login",
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
+  res.setHeader("Set-Cookie", "loggedIn=true");
   req.isLoggedIn = true;
-  res.redirect('/');
+  res.redirect("/");
 };
-
