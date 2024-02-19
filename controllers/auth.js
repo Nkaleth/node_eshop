c33b6d31c8838a06ac3d4eeaf37fed47ce35e3de
Nkaleth <nseguralu@gmail.com>
2024-02-19 17:59:19 -0500

    Install mongodb-session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn =
   //   req.get("Cookie").split(";")[0].trim().split("=")[1] === "true";
+  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
@@ -9,7 +10,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
-  req.isLoggedIn = true;
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
