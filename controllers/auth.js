b53c50df967eace1917826e1568893035b8473cb
Nkaleth <nseguralu@gmail.com>
2024-02-23 16:06:54 -0500

    ADD message on signUp page if the email already exists

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,9 +16,16 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
+    errorMessage: message,
   });
 };
 
@@ -59,6 +66,10 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash(
+          "error",
+          "E-mail exists already, please pick a different one"
+        );
         return res.redirect("/signup");
       }
       return bcrypt
