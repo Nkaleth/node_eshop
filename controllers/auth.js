3182b422e2510501b66bbd802f7655f53381da8e
Nkaleth <nseguralu@gmail.com>
2024-02-26 23:35:33 -0500

    FIX typo on the error value for the feedback message and add route for postReset

--- controllers/auth.js
+++ controllers/auth.js
@@ -148,7 +148,7 @@ exports.postReset = (req, res, next) => {
     User.findOne({ email: req.body.email })
       .then((user) => {
         if (!user) {
-          req.flash("errr", "No account with taht email found.");
+          req.flash("error", "No account with that email found.");
           return res.redirect("/reset");
         }
         user.resetToken = token;
