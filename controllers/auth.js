79dad96dee9517fb37c1bd9c7f0404b905381bc2
Nkaleth <nseguralu@gmail.com>
2024-02-29 12:00:52 -0500

    ADD express-validator package and configure a validation for email. First import check and validationResult on auth route and controller. Then add a the middleware on the route /signup to check the email a get a specific message

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,8 @@ const mailchimpTx = require("@mailchimp/mailchimp_transactional")(
   process.env.MAILCHIMP_API_KEY
 );
 
+const { validationResult } = require("express-validator");
+
 async function welcomeEmail(message) {
   const response = await mailchimpTx.messages.send({
     message,
@@ -75,6 +77,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
