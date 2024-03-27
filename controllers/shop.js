2685407b5b09b739cb8a79912b6e3cae9bcc34cc
Nkaleth <nseguralu@gmail.com>
2024-03-26 19:37:22 -0500

    ADD pagination with pagination.ejs includes also in product-list view

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,18 +4,32 @@ const Product = require("../models/product");
 const Order = require("../models/order");
 
 const PDFDocument = require("pdfkit");
-const product = require("../models/product");
 
 const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render("shop/product-list", {
         prods: products,
-        pageTitle: "All Products",
+        pageTitle: "Products",
         path: "/products",
-        isAuthenticated: req.session.isLoggedIn,
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
